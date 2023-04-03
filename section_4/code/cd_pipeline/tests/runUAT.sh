64cdf35ea1e2d62f38928da45266a914309adc97
Laurentiu <65120497+laur211@users.noreply.github.com>
2023-04-03 09:48:51 +0300

    Update runUAT.sh

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -8,7 +8,7 @@ port=$1
 sleep 5 
 
 # ping the app
-status_code=$(curl -w %{http_code} -s -o ${hostname}:${port})
+status_code=$(curl -s -o -w %{http_code} ${hostname}:${port})
 echo $status_code
 
 if [ $status_code == 200 ];
